ac32752df93857fa5bc5bbe31ef28ea0032ae8aa
Olena Spivakova <alena.spivakova1@gmail.com>
2023-04-29 10:45:59 +0200

    fix

--- src/index.js
+++ src/index.js
@@ -15,6 +15,12 @@ searchBox.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 function onSearch(evt) {
   const searchValue = evt.target.value.trim();
 
+  if (!searchValue) {
+    countryInfo.innerHTML = '';
+    countryList.innerHTML = '';
+    return;
+  }
+
   fetchCountries(searchValue)
     .then(countries => {
       if (countries.length > 10) {
@@ -41,12 +47,10 @@ function renderCountriesList(arr) {
     .map(
       ({ name, flags: { svg } }) =>
         `
-    
         <li class="country-item">
         <img class="country-img" src=${svg} alt="${name}" />
          <h2 class="country-name">${name}</h2>
         </li>
-     
 `
     )
     .join('');
@@ -59,12 +63,9 @@ function renderCountryCard(arr) {
     .map(
       ({ name, capital, population, flags: { svg }, languages }) =>
         `<div class="country-title">
-
            <img class="country-img" src=${svg} alt="${name}" />
       <h2 class="country-name">${name}</h2>
-
         </div>
-     
       <ul class="country-discription">
         <li class="item-discription"><span class="title-discrip">Capital:</span>${capital}</li>
         <li class="item-discription"><span class="title-discrip">Population:</span>${population}</li>
